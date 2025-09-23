10b6feef873a4dc42739395800364740a6bf71e1
faseian <nab4nkert@outlook.com>
2025-09-23 14:48:47 -0400

    Initial commit

---
+++ src/main/java/com/pretransfer/Pre_Transfer/PreTransferApplication.java
@@ -0,0 +1,13 @@
+package com.pretransfer.Pre_Transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pretransfer/Pre_Transfer/PreTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pretransfer.Pre_Transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
